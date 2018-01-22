0b7d433c0a89875623f09882354dd970c854de35
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2018-01-22 21:31:26 +0100

    Modules can re-export other modules

---
+++ Sources/PSPDFKitSwift/Exported.swift
@@ -0,0 +1,7 @@
+//
+// Modules can "re-export" other modules
+// https://github.com/apple/swift/blob/master/docs/Modules.rst#modules-can-re-export-other-modules
+//
+
+@_exported import PSPDFKit
+@_exported import PSPDFKitUI
