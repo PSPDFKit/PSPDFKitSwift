dbca00462800314cd6f34eba7f217024d8137d9f
Rad Azzouz <rad@pspdfkit.com>
2018-04-03 08:43:59 -0400

    Remove extension Sequence now that we’re using Swift 4.1

--- Sources/PSPDFKitSwift/Helpers.swift
+++ Sources/PSPDFKitSwift/Helpers.swift
@@ -18,15 +18,3 @@ func NSUnimplemented(_ fn: String = #function, file: StaticString = #file, line:
 func NSRequiresConcreteImplementation(_ fn: String = #function, file: StaticString = #file, line: UInt = #line) -> Never {
     fatalError("\(fn) must be overriden in subclass implementations", file: file, line: line)
 }
-
-#if swift(>=4.1)
-// TODO: remove this file when Xcode 9.2 is no longer used
-#else
-extension Sequence {
-    public func compactMap<ElementOfResult>(
-        _ transform: (Element) throws -> ElementOfResult?
-        ) rethrows -> [ElementOfResult] {
-        return try flatMap(transform)
-    }
-}
-#endif
